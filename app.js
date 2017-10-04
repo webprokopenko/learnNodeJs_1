bf5af23e3e3e5cd4c5a45d135c1e9918ccedb3dc
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-04 23:46:03 +0300

    Require standard modules

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var greet  = require('./modules');
 
+//https://nodejs.org/dist/latest-v8.x/docs/api/util.html 
+var util = require('util');
+
+
+
+var name = 'Ivan';
+var greeting = util.format('Hello ,%s ',name);
+util.log(greeting);
+
 greet.english();
 greet.spanish();
 
