a366478378ac1496e50416fb6bf71172062c3775
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-05 14:44:28 +0300

    Event emmiter undefined undefined

---
+++ eventEmmiter.js
@@ -0,0 +1,18 @@
+var util = require('util');
+function Person(){
+    this.firstname = "John",
+    this.lastname = 'Doe'
+};
+
+Person.prototype.greet = function(){
+    console.log(`Congratulation ${this.firstname} ${this.lastname}`);
+}
+
+function Policeman(){
+    this.badgenumber = 123;
+}
+
+util.inherits(Policeman,Person);
+
+var Officer = new Policeman();
+Officer.greet();
