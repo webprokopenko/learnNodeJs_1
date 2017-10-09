04571b8999bcd7b693511b5b66bf1c50515af4f5
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-09 11:37:58 +0300

    HTML and Templates

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-var greet  = require('./modules');
+var fs = require('fs');
 
 var http = require('http');
 http.createServer(function(req, res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello world\n');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    var html = fs.readFileSync(__dirname + '/index.html','utf8');
+    var message = "HELLO WORLD!!!";
+    html = html.replace('{Message}',message);
+    res.end(html);
 
 }).listen(1337,'127.0.0.1');
