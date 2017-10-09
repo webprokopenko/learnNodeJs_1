c4ff9dd2d7c4de1ed46098538b72d2953db1df0f
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-09 11:20:00 +0300

    Build a Web Server

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
 var greet  = require('./modules');
 
-//https://nodejs.org/dist/latest-v8.x/docs/api/util.html 
-var util = require('util');
+var http = require('http');
+http.createServer(function(req, res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello world\n');
 
-
-
-var name = 'Ivan';
-var greeting = util.format('Hello ,%s ',name);
-util.log(greeting);
-
-greet.english();
-greet.spanish();
-
-
-var Emmiter = require('./emmiter.js');
-var emtr = new Emmiter();
-emtr.on('greet',function(){
-    console.log('Somewhere someone say Hello');
-});
-emtr.on('greet',function(){
-    console.log('A greeting occured');
-});
-
-emtr.emit('greet');
-
-var EmmiterNode = require('events');
-var eventConfig = require('./config.js').events;
-
-var emtrNode = new EmmiterNode();
-emtrNode.on(eventConfig.GREET,function(){
-    console.log('Somewhere someone say Hello');
-});
-emtrNode.on(eventConfig.GREET,function(){
-    console.log('A greeting occured');
-});
-
-emtrNode.emit(eventConfig.GREET);
+}).listen(1337,'127.0.0.1');
