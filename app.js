71fc6df6bc6bae0569b9eedcc2fc8703e06f81fd
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-09 12:06:02 +0300

    Routing

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ var fs = require('fs');
 
 var http = require('http');
 http.createServer(function(req, res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    var html = fs.createReadStream(__dirname + '/index.html').pipe(res);
-    
+
+    if(req.url === '/api'){
+        res.writeHead(200,{'Content-Type':'application/json'});
+        
+            var obj = {
+                firstname: 'John',
+                lastname: 'Caddy'
+            };
+        
+            res.end(JSON.stringify(obj));
+    }
+    else if(req.url === '/'){
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+    else{
+        res.writeHead(404);
+        res.end();
+    }
 
 }).listen(1337,'127.0.0.1');
