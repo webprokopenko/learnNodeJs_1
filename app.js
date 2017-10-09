3d54105d30378b25cc8eeaace9034b6bfb91f114
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-09 11:44:25 +0300

    Streams and Performance

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ var fs = require('fs');
 var http = require('http');
 http.createServer(function(req, res){
     res.writeHead(200,{'Content-Type':'text/html'});
-    var html = fs.readFileSync(__dirname + '/index.html','utf8');
-    var message = "HELLO WORLD!!!";
-    html = html.replace('{Message}',message);
-    res.end(html);
+    var html = fs.createReadStream(__dirname + '/index.html').pipe(res);
+    
 
 }).listen(1337,'127.0.0.1');
