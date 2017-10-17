5fbd3c4fa442f830eff22a4c8e2bafb8f199f6c8
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-17 12:26:39 +0300

    Send data on a template

--- express1/app.js
+++ express1/app.js
@@ -18,7 +18,7 @@ app.get('/',function(req,res){
 });
 
 app.get('/person/:id',function(req,res){
-    res.send('<html><head></head><body><h1>Person:'+ req.params.id +'</h1></body></html>');
+    res.render('person',{ID:req.params.id});
 });
 
 app.get('/api',function(req,res){
