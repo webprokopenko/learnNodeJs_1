4bca3745bb34a28634921e76904a1ad39e12d1d0
Ivan Prokopenko <web.prokopenko@gmail.com>
2017-10-17 12:14:10 +0300

    Install ejs in express1

--- express1/app.js
+++ express1/app.js
@@ -4,6 +4,9 @@ var port = process.env.PORT || 3000;
 
 app.use('/assets',express.static(__dirname + '/public'));
 
+app.set('view engine','ejs');
+
+
 app.use('/',function(req,res,next){
     console.log('Request URL: '+req.url);
     next();
